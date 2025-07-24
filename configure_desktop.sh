4e79c7dfcda7ca769bc12498ba5684a8f697768a
1FIL0 <serglinux2@gmail.com>
2025-07-24 16:11:16 +0100

    add laptop and desktop modes

---
+++ configure_desktop.sh
@@ -0,0 +1,1 @@
+cp ~/.local/share/polybar/themes/nimbledesktop/bars_desktop.ini ~/.local/share/polybar/themes/nimbledesktop/bars.ini
---
+++ configure_laptop.sh
@@ -0,0 +1,1 @@
+cp ~/.local/share/polybar/themes/nimbledesktop/bars_laptop.ini ~/.local/share/polybar/themes/nimbledesktop/bars.ini
